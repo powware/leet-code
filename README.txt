bb20580ef75da65733a3ec3ea023673b0e46ea07
powware <powwared@gmail.com>
2024-08-28 13:01:04 +0200

    121

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 https://seanprashad.com/leetcode-patterns/
 https://www.designgurus.io/course/grokking-the-coding-interview
+https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions
---
+++ src/121_best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+
+#include <gtest/gtest.h>
+
+class Solution
+{
+public:
+    int maxProfit(std::vector<int> &prices)
+    {
+        int value = prices[0];
+        int max_profit = 0;
+        int day = 0;
+        for (std::size_t i = 1; i < prices.size(); ++i)
+        {
+            auto profit = prices[i] - value;
+            if (profit < 0)
+            {
+                value = prices[i];
+            }
+            if (profit > max_profit)
+            {
+                max_profit = profit;
+                day = i;
+            }
+        }
+
+        return day ? day + 1 : 0;
+    }
+};
+
+void BestTimeToBuyAndSellStockCommon(std::vector<int> prices, int expected)
+{
+    Solution solution;
+
+    ASSERT_EQ(solution.maxProfit(prices), expected);
+}
+
+TEST(BestTimeToBuyAndSellStock, Case1)
+{
+    BestTimeToBuyAndSellStockCommon({7, 1, 5, 3, 6, 4}, 5);
+}
+
+TEST(BestTimeToBuyAndSellStock, Case2)
+{
+    BestTimeToBuyAndSellStockCommon({7, 6, 4, 3, 1}, 0);
+}
+
+TEST(BestTimeToBuyAndSellStock, Case3)
+{
+    BestTimeToBuyAndSellStockCommon({7, 6, 5, 4, 3, 7, 6, 5, 4, 3, 2, 1, 6, 5, 4, 3, 2, 8, 1, 2, 3, 4, 5, 6, 7}, 18);
+}
+
+TEST(BestTimeToBuyAndSellStock, Case4)
+{
+    BestTimeToBuyAndSellStockCommon({2, 7, 1, 4, 3, 1}, 2);
+}
