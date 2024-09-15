6ada4282bd189fec8080001270fc317174282be2
powware <powwared@gmail.com>
2024-09-15 13:05:40 +0200

    202

---
+++ src/202_happy_number.cpp
@@ -0,0 +1,74 @@
+#include <array>
+#include <cmath>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isHappy(int n)
+    {
+        int slow = n;
+        int fast = n;
+
+        while (fast != 1)
+        {
+            slow = SumDigitsSquared(slow);
+            fast = SumDigitsSquared(fast);
+            if (fast == 1)
+            {
+                return true;
+            }
+
+            fast = SumDigitsSquared(fast);
+            if (fast == slow)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+private:
+    static inline int SumDigitsSquared(int n)
+    {
+        int sum = 0;
+        while (n)
+        {
+            sum += std::pow(n % 10, 2);
+            n /= 10;
+        }
+
+        return sum;
+    }
+};
+
+void HappyNumberCommon(int n, bool expected)
+{
+    Solution solution;
+
+    ASSERT_EQ(solution.isHappy(n), expected);
+}
+
+TEST(HappyNumber, Case0)
+{
+    HappyNumberCommon(7, true);
+}
+
+TEST(HappyNumber, Case5)
+{
+    HappyNumberCommon(192352347, false);
+}
+
+TEST(HappyNumber, Case1)
+{
+    HappyNumberCommon(19, true);
+}
+
+TEST(HappyNumber, Case2)
+{
+    HappyNumberCommon(2, false);
+}
