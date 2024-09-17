40f3f0f1b0fe14fd5bd97d9e921da3a5925ceb67
powware <powwared@gmail.com>
2024-09-17 17:17:17 +0200

    better but still far from optimal

--- src/127_word_letter.cpp
+++ src/127_word_letter.cpp
@@ -1,7 +1,8 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 #include <queue>
+#include <numeric>
 
 #include <gtest/gtest.h>
 
@@ -10,45 +11,68 @@ class Solution
 public:
     int ladderLength(std::string beginWord, std::string endWord, std::vector<std::string> &wordList)
     {
-        std::queue<std::pair<std::string, int>> next;
-
+        std::queue<std::pair<std::size_t, int>> next;
+        std::unordered_set<std::size_t> set;
         const auto n = wordList.size();
         const auto m = beginWord.size();
 
-        next.emplace(std::move(beginWord), 0);
+        for (std::size_t i = 0; i < n; ++i)
+        {
+            set.emplace(i);
+            std::size_t differs = 0;
+            for (std::size_t j = 0; j < m; ++j)
+            {
+                if (beginWord[j] != wordList[i][j])
+                {
+                    ++differs;
+                }
+            }
+            if (differs == 0)
+            {
+                next.emplace(i, 0);
+            }
+            else if (differs == 1)
+            {
+                if (wordList[i] == endWord)
+                {
+                    return 2;
+                }
+                next.emplace(i, 1);
+            }
+        }
 
         while (not next.empty())
         {
-            auto current = std::move(std::get<0>(next.front()));
-            auto path = std::get<1>(next.front());
+            auto [index, path] = next.front();
             next.pop();
             ++path;
 
-            for (std::size_t i = 0; i < n; ++i)
+            std::vector<std::size_t> remove;
+            for (auto it = set.begin(); it != set.end();)
             {
-                if (wordList[i].empty())
-                {
-                    continue;
-                }
-                auto it = wordList[i];
 
                 std::size_t differs = 0;
                 for (std::size_t j = 0; j < m; ++j)
                 {
-                    if (current[j] != it[j])
+                    if (wordList[index][j] != wordList[*it][j])
                     {
                         ++differs;
                     }
                 }
                 if (differs == 1)
                 {
-                    if (it == endWord)
+                    if (wordList[*it] == endWord)
                     {
                         return path + 1;
                     }
 
-                    next.emplace(std::move(it), path);
-                    wordList[i].clear();
+                    next.emplace(*it, path);
+                    remove.push_back(*it);
+                    it = set.erase(it);
+                }
+                else
+                {
+                    ++it;
                 }
             }
         }
@@ -62,17 +86,27 @@ void WordLadderCommon(std::string beginWord, std::string endWord, std::vector<st
     ASSERT_EQ(solution.ladderLength(beginWord, endWord, wordList), expected);
 }
 
-TEST(WordLadder, Case1)
-{
-    WordLadderCommon("hit", "cog", {"hot", "dot", "dog", "lot", "log", "cog"}, 5);
-}
+// TEST(WordLadder, Case1)
+// {
+//     WordLadderCommon("hit", "cog", {"hot", "dot", "dog", "lot", "log", "cog"}, 5);
+// }
 
-TEST(WordLadder, Case2)
-{
-    WordLadderCommon("hit", "cog", {"hot", "dot", "dog", "lot", "log"}, 0);
-}
+// TEST(WordLadder, Case2)
+// {
+//     WordLadderCommon("hit", "cog", {"hot", "dot", "dog", "lot", "log"}, 0);
+// }
 
-TEST(WordLadder, Case3)
+// TEST(WordLadder, Case3)
+// {
+//     WordLadderCommon("a", "c", {"a", "b", "c"}, 2);
+// }
+
+// TEST(WordLadder, Case4)
+// {
+//     WordLadderCommon("hot", "dog", {"hot", "dog"}, 0);
+// }
+
+TEST(WordLadder, Case5)
 {
-    WordLadderCommon("a", "c", {"a", "b", "c"}, 2);
+    WordLadderCommon("lost", "cost", {"most", "fist", "lost", "cost", "fish"}, 2);
 }
